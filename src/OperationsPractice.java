21aa1e3c571e41cccef4d12911f0e0a8976b4e5e
agokhale22 <43002655+agokhale22@users.noreply.github.com>
2018-10-01 08:32:34 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,22 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number; // data type then variable name
+		number = 3;
+		number +=5; // shortcut for number = number + 5
+System.out.println(" When you add 5 the anser is "  +  number);
+	number /=2;
+	System.out.println("when you divide by two "  + number); //number = number / 2
+	number *=4;
+	System.out.println(" when you multiply by 4 the answer is "  + number);
+	number -=6;
+	System.out.println("when you subtract 6 the answer is "  + number);
+	number %=2;
+	System.out.println("when you modularly divide by 2 "  + number);		
+	
+	}
+
+}
